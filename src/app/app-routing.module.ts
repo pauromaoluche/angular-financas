6422c6f0cf0684ffc3adef6d8da91811f62481e3
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2022-06-02 14:44:39 -0300

    Update: Adding categories module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /* loadChildren, carrega o modulo especifico do caminho */
+  {
+    path: 'categories',
+    // loadChildren: './pages/categories/categories.module#CategoriesModule'
+    loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
