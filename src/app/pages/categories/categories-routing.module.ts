da8844d6ca254afee30b3a40273a7b60457eeb10
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2022-06-02 23:51:01 -0300

    Update: improving category list component template

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -6,7 +6,7 @@ import { CategoryFormComponent } from './category-form/category-form.component';
 const routes: Routes = [
   { path: '', component: CategoryListComponent },
   { path: 'new', component: CategoryFormComponent},
-  { path: ':id', component: CategoryFormComponent},
+  { path: ':id/edit', component: CategoryFormComponent},
   {}
 ];
 
