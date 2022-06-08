6228caf0087ee42564a7f4195598f891e67aa5db
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2022-06-08 10:46:51 -0300

    Update: Adding CategoryFormComponent

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -44,7 +44,7 @@ export class CategoryFormComponent implements OnInit {
     this.loadCategory();
   }
 
-  ngAfterContentChecked() { 
+  ngAfterContentChecked() {
     this.setPageTitle();
   }
 
@@ -63,8 +63,8 @@ export class CategoryFormComponent implements OnInit {
   private buildCategoryForm() {
     this.categoryForm = this.formBuilder.group({
       id: [null],
-      name: [null, [Validators.required, Validators.minLength(2)]],
-      description: [null]
+      name: [null, [Validators.required, Validators.minLength(5)]],
+      description: [null, [Validators.required, Validators.minLength(5)]]
     })
   }
 
@@ -74,22 +74,73 @@ export class CategoryFormComponent implements OnInit {
       this.route.paramMap.pipe(
         switchMap(params => this.categoryService.getByid(+params.get('id')!))
       ).subscribe(
-          (category) => {
-            this.category = category;
-            this.categoryForm.patchValue(category) // binds loaded category data to CategoryForm
-          },
-          (error) => alert('Ocorreu um erro no servidor, tente mais tarde.')
-        )
+        (category) => {
+          this.category = category;
+          this.categoryForm.patchValue(category) // binds loaded category data to CategoryForm
+        },
+        (error) => alert('Ocorreu um erro no servidor, tente mais tarde.')
+      )
     }
   }
 
-  private setPageTitle(){
-    if(this.currentAction == 'new'){
+  private setPageTitle() {
+    if (this.currentAction == 'new') {
       this.pageTitle = 'Cadastro de nova categoria!';
-    }else{
+    } else {
       /* verifica o nome da categoria, se nao tiver, ele passa ' ' */
       const categoryName = this.category.name || '';
-      this.pageTitle = "Editando Categoria: "+ categoryName;
+      this.pageTitle = "Editando Categoria: " + categoryName;
     }
+  }
+
+  submitForm() {
+    this.submittingForm = true;
+
+    if (this.currentAction == 'new') {
+      this.createCategory();
+    } else {
+      this.updateCategory();
+    }
+  }
+
+  private createCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category).subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private updateCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+    /* o id ja esta vindo dentro de category entao nao precisamos passar o id da category aqui, duvidas só consultar o 
+    category.service.ts */
+    this.categoryService.update(category).subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private actionsForSuccess(category: Category) {
+    toastr.success("Solicitação processada com sucesso!");
+
+    /* skipLocationChange, faz com que não adiciona a url no historico do navegador */
+    this.router.navigateByUrl('categories', { skipLocationChange: true }).then(
+      /* primeiro ele rediciona para o /categories, e depois manda para categories/id/edit */
+      () => this.router.navigate(["categories", category.id, "edit"])
+    )
+  }
+
+  private actionsForError(error: any){
+    toastr.error("Ocorreu um erro ao processar sua solicitação");
+    this.submittingForm = false;
+
+    if(error.status === 422){
+      this.serverErrorMessages = JSON.parse(error._body).errors
+    }else{
+      this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor, tenta mais tarde!.']
+    }
+
   }
 }
